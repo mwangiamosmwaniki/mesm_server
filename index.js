e96a8889f0d5b8a06c218d6ae29f041f72609240
Amosjnr1225 <mwangiamos703@gmail.com>
2025-08-25 18:20:52 +0300

    final update

--- index.js
+++ index.js
@@ -65,7 +65,9 @@ app.post("/send-email", async (req, res) => {
             
             <!-- Header with Logo -->
             <div style="background: linear-gradient(90deg, #3498db, #2ecc71); padding: 20px; text-align: center; color: #fff;">
-              <img src="assets\mesm.png" alt="MESM Logo" style="max-width: 120px; margin-bottom: 10px;" />
+             <img src="https://mesm.netlify.app/mesm.png" 
+              alt="MESM Logo" 
+              style="max-width: 120px; margin-bottom: 10px;" />
               <h1 style="margin: 0;">Thank You, ${name}!</h1>
             </div>
             
